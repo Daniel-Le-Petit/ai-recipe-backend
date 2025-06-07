999f0a3e66310126348c7c8cca6500f1be5b4a79
Daniel Le Petit <dlepetit@hotmail.fr>
2025-06-07 16:43:12 +0200

    Configure database.js to use DATABASE_URL directly with SSL

--- backend-js/config/database.js
+++ backend-js/config/database.js
@@ -1,37 +1,17 @@
-// backend-js/config/database.js
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('DATABASE_HOST', 'localhost'),
-//       port: env.int('DATABASE_PORT', 5432),
-//       database: env('DATABASE_NAME', 'aifinesherbes'),
-//       user: env('DATABASE_USERNAME', 'postgres'),
-//       password: env('DATABASE_PASSWORD', 'votre_mot_de_passe'),
-//       ssl: false,
-//     },
-//   },
-// });
+const path = require('path');
 
-const parse = require('pg-connection-string').parse;
-
-module.exports = ({ env }) => {
-  const config = parse(env('DATABASE_URL'));
-
-  return {
-    connection: {
-      client: 'postgres',
-      connection: {
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        user: config.user,
-        password: config.password,
-        ssl: env.bool('DATABASE_SSL', false) && {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres', // Or 'sqlite' etc., based on your client
+    connection: env('DATABASE_URL'), // Use the DATABASE_URL directly
+    useNullAsDefault: true, // Common for SQLite, but might be needed for others
+    ssl: env.bool('DATABASE_SSL', false) && {
           rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
-      },
-      debug: false,
     },
-  };
-};
+    pool: {
+      min: 0,
+      max: 10,
+    },
+    debug: false,
+  },
+});
