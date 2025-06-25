92b9c2d9c791f149524321c3fed8c3676e9aacd7
Daniel Le Petit <dlepetit@hotmail.fr>
2025-06-25 15:09:07 +0200

    feat: changement cards Site Matthieu frontend-m maj relation RecipeUser

--- backend-js/config/database.js
+++ backend-js/config/database.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: env('DATABASE_CLIENT', 'postgres'),
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
